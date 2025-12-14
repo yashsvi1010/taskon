a3cb40f6dec70b73752660d0e2a8b52ee8cf4598
yashsvi1010 <yashsvisingh97@gmail.com>
2025-12-14 22:01:54 +0530

    added delete & edit functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { FaRegEdit } from "react-icons/fa";
 import { MdOutlineDeleteOutline } from "react-icons/md";
 import { MdDone } from "react-icons/md";
@@ -12,6 +12,9 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
   const handleDone = (id: number) => {
     setTodos(
       todos.map((todo) =>
@@ -20,20 +23,51 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
     );
   };
 
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
   return (
     <div>
-      <form className="todos__single">
-        {todo.isDone ? (
+      <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+        {edit ? (
+          <input
+            ref={inputRef}
+            value={editTodo}
+            onChange={(e) => setEditTodo(e.target.value)}
+            className="todos__single--text"
+          />
+        ) : todo.isDone ? (
           <s className="todos__single--text">{todo.todo}</s>
         ) : (
           <span className="todos__single--text">{todo.todo}</span>
         )}
 
         <div>
-          <span className="icon">
+          <span
+            className="icon"
+            onClick={() => {
+              if (!edit && !todo.isDone) {
+                setEdit(!edit);
+              }
+            }}
+          >
             <FaRegEdit />
           </span>
-          <span className="icon">
+          <span className="icon" onClick={() => handleDelete(todo.id)}>
             <MdOutlineDeleteOutline />
           </span>
           <span className="icon" onClick={() => handleDone(todo.id)}>
